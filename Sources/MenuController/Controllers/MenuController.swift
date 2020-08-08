e75a4f2266ae792776619d3be0330673afef1c77
Anmol Rajpal <anmol.rajpal@me.com>
2020-08-08 16:08:39 +0530

    Added support for adding header view and header height in Menu Controller

--- Sources/MenuController/Controllers/MenuController.swift
+++ Sources/MenuController/Controllers/MenuController.swift
@@ -10,10 +10,7 @@ import UIKit
 import InteractiveModal
 
 public class MenuController:InteractiveModalViewController {
-    public required init(actions:[UIControlMenuAction]) {
-        super.init(controller: UIControlMenuController(headerView: nil, headerHeight: 54.0, actions: actions))
-    }
-    init(headerView:UIView? = nil, headerHeight:CGFloat = 54.0, actions: [UIControlMenuAction]) {
+    public required init(headerView:UIView? = nil, headerHeight:CGFloat = 54.0, actions:[UIControlMenuAction]) {
         super.init(controller: UIControlMenuController(headerView: headerView, headerHeight: headerHeight, actions: actions))
     }
 }
